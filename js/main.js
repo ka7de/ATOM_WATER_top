d68eaa3265e8d17ea542beeaacb56144c66459b0
ka7de <133555923+ka7de@users.noreply.github.com>
2023-07-23 17:02:54 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function (){
+    // header_accordion
+    $('#accordion-item').on('click',function(){
+        $('#accordion-list').slideToggle();
+        $('#accordion').toggleClass('open');
+    });
+
+    // hamburger
+    $('#hamburger').on('click',function(){
+        $('#hamburger span').toggleClass('show');
+        $('nav').toggleClass('show');
+        $('#mask').toggleClass('show');
+
+        if($('#slidedown').hasClass('open')){
+            $('#slidedown').removeClass('open');
+            $('#slideitem').slideUp();
+        }
+    });
+
+    $('nav ul li a').on('click',function(){
+        $('#hamburger span').removeClass('show');
+        $('nav').removeClass('show');
+        $('#mask').removeClass('show');
+    });
+
+    $('#mask').on('click',function(){
+        $(this).removeClass('show');
+        $('#hamburger span').removeClass('show');
+        $('nav').removeClass('show');
+    });
+
+    $('#slidedown').on('click',function(){
+        let slideItem = $(this).next('#slideitem');
+        slideItem.slideToggle();
+        $(this).toggleClass('open');
+    });
+});
