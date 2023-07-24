7ca2c93ee7a3d709f57f7d52537d218a211aa9ce
ka7de <133555923+ka7de@users.noreply.github.com>
2023-07-24 16:27:17 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,4 +39,22 @@ $(function (){
         slideItem.slideToggle();
         $(this).toggleClass('open');
     });
+
+    // トップへスクロール
+    $(function(){
+        var topBtn = $('#top');
+        topBtn.hide();
+        $(window).scroll(function(){
+            if($(this).scrollTop() > 500){
+                topBtn.fadeIn();
+            } else {
+                topBtn.fadeOut();
+            }
+        });
+    
+        topBtn.click(function(){
+            $('body,html').animate({'scrollTop':0},500);
+            return false;
+        });
+    });
 });
