cc219e13c0976a19e89c52228069fa01b8a85470
ka7de <133555923+ka7de@users.noreply.github.com>
2023-07-27 18:55:20 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,4 +57,59 @@ $(function (){
             return false;
         });
     });
+
+    // スムーススクロール
+$(function(){
+    $('a[href^="#"]').click(function(){
+        var speed = 500;
+        var type = 'swing';
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top -80;
+        $('body,html').animate({'scrollTop':position},speed,type);
+        return false;
+    });
+});
+
+    // エラーメッセージ
+    // $('#submit').on('click',function(){
+    //     $('#company-name-text').text("");
+    //     $('#name-text').text("");
+    //     $('#mail-text').text("");
+    //     $('#contents-desc').text("");
+
+    //     if(('input[name = "name"]').val() == ""){
+    //         $('#name-text').text("名前は必須項目です。");
+    //     }
+
+    //     if(('input[name = "mail"]').val() == ""){
+    //         $('#mail-text').text('メールアドレスは必須項目です。');
+    //     }
+
+    //     if(('input[name = "contents"]').val() == ""){
+    //         $('#contents-desc').text('内容は必須項目です。');
+    //     }
+    // });
+
+    $(function(){
+        const hash = location.hash;
+
+        if(hash){
+            $("html,body").stop().scrollTop(0);
+            setTimeout(function(){
+                const target = $(hash);
+                position = target.offset().top;
+                $("html,body").animate({scrollTop:position},500,"swing");
+            });
+        }
+    });
+
+    function check(){
+        if(mail_form.mail.value=""){
+            alert('メールアドレスを入力してください');
+            return false;
+        } else {
+            return true;
+        }
+    };
 });
